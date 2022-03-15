e3e8d432f148cfbb2aa03a01a8689df628040899
Bira <bthiban@gmail.com>
2022-03-15 08:35:14 +0800

    Update test.java

--- test.java
+++ test.java
@@ -56,3 +56,18 @@ class Child extends Parent {
 }
 
 //-----
+public class Test {
+ 
+     private static void add(double d1, double d2) {
+         System.out.println("double version: " + (d1 + d2));
+     }
+ 
+     private static void add(Double d1, Double d2) {
+         System.out.println("Double version: " + (d1 + d2));
+     }
+ 
+     public static void main(String[] args) {
+         add(10.0, new Integer(10));
+     }
+ 
+}
